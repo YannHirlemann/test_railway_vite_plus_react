4dc4e5809cd4824250713d2809d2c7d157c620d2
Yann Hirlemann <yann.hirlemann@oclock.io>
2023-06-22 14:11:28 +0200

    add react-router-dom and contact page

---
+++ src/Contact.jsx
@@ -0,0 +1,15 @@
+// contact component
+import { Link } from "react-router-dom";
+const Contact = () => {
+ return (
+  <>
+   <div className="contact">
+    <h1>Contact</h1>
+    <p>
+     <Link to="/">Home</Link>
+    </p>
+   </div>
+  </>
+ );
+};
+export default Contact;
--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,24 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
-import './index.css'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
 
-ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-)
+import App from "./App.jsx";
+import Contact from "./Contact.jsx";
+import "./index.css";
+
+const router = createBrowserRouter([
+ {
+  path: "/",
+  element: <App />,
+ },
+ {
+  path: "/contact",
+  element: <Contact />,
+ },
+]);
+
+ReactDOM.createRoot(document.getElementById("root")).render(
+ <React.StrictMode>
+  <RouterProvider router={router} />
+ </React.StrictMode>
+);
